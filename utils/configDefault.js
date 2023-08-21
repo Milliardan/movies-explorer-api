cc4f27fd944140badc435c0c988d59e8b316aa02
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-08-21 20:45:55 +0200

    аутентификация, авторизация, обработка ошибок, логирование

--- utils/configDefault.js
+++ utils/configDefault.js
@@ -2,7 +2,7 @@ const PORT = 3001;
 const DATABASE_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const JWT_SECRET = 'secret-pas';
 const SALT_LENGTH = 10;
-const LIMITER_WINDOW = 60000;
+const LIMITER_WINDOW = 70000;
 const LIMITER_MAX_LIMIT = 100;
 
 module.exports = {
