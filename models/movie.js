82c2e6274a5f217d2be4ceaf48b79ef3cab7405c
Denis Kezhvatov <kezhvatovdenis@yandex.ru>
2023-08-22 11:43:10 +0200

    fix: убрать уникальность поля movieId

--- models/movie.js
+++ models/movie.js
@@ -55,7 +55,6 @@ const movieSchema = new mongoose.Schema(
     movieId: {
       type: Number,
       required: true,
-      unique: true,
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
